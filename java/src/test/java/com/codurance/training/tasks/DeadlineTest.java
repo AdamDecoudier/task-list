004dc0820bf945317cb6c6badd3ffe349745c636
Adam <adamdecoudier@gmail.com>
2020-06-29 14:29:42 +0200

    add deadline test

---
+++ java/src/test/java/com/codurance/training/tasks/DeadlineTest.java
@@ -0,0 +1,21 @@
+package com.codurance.training.tasks;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+
+    @Test
+    public void getIdTest(){
+        Deadline deadline = new Deadline;
+        deadline.setId(1L);
+        assertEquals(1L, deadline.getId());
+    }
+
+    @Test
+    public void getDateTest(){
+        Deadline deadline = new Deadline;
+        deadline.setDate(1L);
+        assertEquals(1L, deadline.getDate());
+    }
+}
